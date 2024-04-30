f480fa967c30b9212f8569aa1e63ddf6d9d8575b
alecioc <alessandro@ciociola.me>
2024-04-30 19:54:13 +0200

    add plotly choropleth with scatter layer example

--- Home.py
+++ Home.py
@@ -2,51 +2,64 @@ import osmnx as ox
 import streamlit as st
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 st.title("Dashboard geospaziale città di Milano")
 
-try:
 
-    # city_name = st.text_input("Inserisci il nome di una città:")
+if True:
+
     city_gdf = ox.geocode_to_gdf("Milano")
-    #city_gdf.crs = "epsg:4326"
-    #city_gdf = city_gdf.to_crs("epsg:3857")
 
-    print(city_gdf.crs)
+    #city_name = st.text_input("Inserisci il nome di una città:")
+    #city_gdf = ox.geocode_to_gdf(city_name)
+
     with st.expander("Clicca per vedere dataset"):
-        st.write(city_gdf)
+        st.write(city_gdf.drop("geometry", axis=1))
 
-    # fig, ax = plt.subplots()
-    # city_gdf.plot(ax=ax)
-    # st.pyplot(fig)
-    #
-    # city_gdf_json = city_gdf.to_json()
-    #
-    # with st.expander("Clicca per vedere GeoJSON"):
-    #     st.write(city_gdf_json)
-    #
-    # import plotly.express as px
-    #
-    # fig = px.choropleth_mapbox(
-    #     city_gdf.drop("geometry", axis=1),
-    #     geojson=city_gdf.to_json(),
-    #     #locations='fips',
-    #     #color='unemp',
-    #     color_continuous_scale="Viridis",
-    #     #range_color=(0, 12),
-    #     mapbox_style="carto-positron",
-    #     zoom=3,
-    #     center = {"lat": 45, "lon": 9},
-    #     opacity=0.5,
-    #     #labels={'unemp':'unemployment rate'}
-    # )
-    # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    # # fig.show()
-    #
-    # st.plotly_chart(fig)
+    fig, ax = plt.subplots()
+    city_gdf.plot(ax=ax)
+    st.pyplot(fig)
 
-except:
+    city_gdf_json = city_gdf.__geo_interface__
+
+    with st.expander("Clicca per vedere GeoJSON"):
+        st.write(city_gdf_json)
+
+    city_gdf["id"] = list(range(len(city_gdf)))
+    city_gdf = city_gdf.drop("geometry", axis=1)
+
+    fig = px.choropleth_mapbox(
+        city_gdf,
+        geojson=city_gdf_json,
+        locations='id',
+        mapbox_style="carto-positron",
+        center={"lat": city_gdf.lat.iloc[0], "lon": city_gdf.lon.iloc[0]},
+        opacity=0.5,
+    )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    scatter_data = {
+        "lat": [45.5, 45.44],  # List of latitudes
+        "lon": [9.2, 9.1],  # List of longitudes
+        #"text": ["Location 1", "Location 2"],  # Optional text for each point
+        "size": [20, 20]  # Optional sizes for each point
+    }
+
+    # Add scatter layer
+    fig.add_scattermapbox(
+        lat=scatter_data["lat"],
+        lon=scatter_data["lon"],
+        mode='markers+text',  # Shows both markers and text
+        #marker=dict(size=scatter_data["size"], color="blue"),  # Customize marker appearance
+        #text=scatter_data["text"],
+        textposition="bottom right"
+    )
+
+    st.plotly_chart(fig)
+
+else:
 
     st.error("Nome della città non valido!")
 
